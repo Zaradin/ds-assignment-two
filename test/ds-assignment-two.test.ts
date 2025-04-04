26c36a31087a2c2c83f5ee22a7e48b952a6859b5
Zaradin <crottyjj@hotmail.com>
2025-04-04 10:46:25 +0100

    Initial commit

---
+++ test/ds-assignment-two.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DsAssignmentTwo from '../lib/ds-assignment-two-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ds-assignment-two-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DsAssignmentTwo.DsAssignmentTwoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
